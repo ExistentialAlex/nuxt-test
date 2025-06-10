29fd5926b828557c0b3c57b1fb954817e913fe88
Alexander Ashwood <alexander.ashwood@sky.uk>
2025-06-10 08:49:10 +0100

    test: add vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,16 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     setupFiles: ['./tests/setup/i18n.setup.ts'],
+    coverage: {
+      provider: 'istanbul',
+      thresholds: {
+        global: {
+          statements: 90,
+          branches: 90,
+          functions: 90,
+          lines: 90,
+        },
+      },
+    },
   },
 });
